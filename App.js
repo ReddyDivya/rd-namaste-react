40e66a0b564d8220a9df886c3b9a38b7e980bd71
Divya <divyareddy0205@gmail.com>
2023-08-18 20:16:41 -0700

    restaurant-component

--- App.js
+++ App.js
@@ -31,37 +31,101 @@ const Header = () => {
     </div>
 }//Header
 
+const resList = [
+    {
+        id: "1001",
+        name : "Santosh Family Dhaba",
+        receipe: "Tomato Soup",
+        rating: "4.0",
+        price : "₹300 for two",
+        deliveryTime: "22 MINS",
+        image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/mkcibe6x1ychvzjkywxk",
+    },
+    {
+        id: "1002",
+        name : "Cafe Niloufer",
+        receipe: "Maska Bun",
+        rating: "4.5",
+        price : "₹400 for two",
+        deliveryTime: "32 MINS",
+        image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/dc9t0kiq7lhsd3pndohf",
+    },
+    {
+        id: "1003",
+        name : "Santosh Family Dhaba",
+        receipe: "Crispy Corn",
+        rating: "4.5",
+        price : "₹560 for two",
+        deliveryTime: "32 MINS",
+        image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/dwqb8nhiop8fvqya0alq",
+    },
+    {
+        id: "1004",
+        name : "Grameen Kulfi",
+        receipe: "Sitaphal Stick Kulfi",
+        rating: "4.6",
+        price : "₹120 for two",
+        deliveryTime: "18 MINS",
+        image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/vu0thsifji9wgfuzfmic",
+    },
+    {
+        id: "1005",
+        name : "Nrs Nandini",
+        receipe: "Idli",
+        rating: "4.4",
+        price : "₹150 for two",
+        deliveryTime: "21 MINS",
+        image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/9fa5ca30c3db622c69cec0326ff63b41",
+    },
+    {
+        id: "1006",
+        name : "Utsav Vegetarian",
+        receipe: "Kaju Paneer Biryani",
+        rating: "4.1",
+        price : "₹300 for two",
+        deliveryTime: "32 MINS",
+        image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/y2lkcxxax3794vepe2se",
+    },
+    {
+        id: "1007",
+        name : "Subbayya Gari Hotel",
+        receipe: "Butta Bojanam",
+        rating: "4.0",
+        price : "₹300 for two",
+        deliveryTime: "24 MINS",
+        image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/iwtocxqjtu57vc7l52h9",
+    },
+]
+
+
 //2. Body Component
 const Body = () => {
-    return <div className="res-container">
+    return <div className="body">
         <div className="res-search">
             Search
         </div>
-        <div className="res-cards">
-            <RestaurantCard/>
-            <RestaurantCard/>
-            <RestaurantCard/>
-            <RestaurantCard/>
-            <RestaurantCard/>
-            <RestaurantCard/>
-            <RestaurantCard/>
-            <RestaurantCard/>
-            <RestaurantCard/>
-            <RestaurantCard/>
-            <RestaurantCard/>
+        <div className="res-container">
+            {
+                resList.map((restaurant) => (
+                    <RestaurantCard key={restaurant.id} resData={restaurant}/>
+                ))
+            }
         </div>
     </div>
 }
 
 //3. Restaurant Card
-const RestaurantCard = () => {
-    return <div className="res-card">
-        <img className="res-card-image" src="https://b.zmtcdn.com/data/dish_photos/06e/c7315872f57926bc2736f20c0e81206e.jpg?fit=around|130:130&crop=130:130;*,*"/>
-        <h3>TCS The Cake Story</h3>
-        <h4>Chocolate Black Forest Cake</h4>
-        <span>⭐⭐⭐⭐</span>
-        <span>₹399</span>
-    </div>
+const RestaurantCard = (props) => {
+    const {resData} = props;
+
+    return (<div className="res-card">
+        <img className="res-logo" src={resData.image}/>
+        <h3>{resData.name}</h3>
+        <h4>{resData.receipe}</h4>
+        <h4>{resData.rating}</h4>
+        <h4>{resData.deliveryTime}</h4>
+        <h4>{resData.price}</h4>
+    </div>);
 }
 
 const AppLayout = () => {
