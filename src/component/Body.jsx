29c7b599e2d4d6bc5c8199f9037f5af813a79fda
Divya <divyareddy0205@gmail.com>
2023-08-19 14:57:34 -0700

    swiggy-api-changes

--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -1,5 +1,5 @@
 import RestaurantCard from "./RestaurantCard";
-import {resList} from "../utils/mockData";
+import {restaurantList} from "../utils/mockData";
 
 //2. Body Component
 const Body = () => {
@@ -9,8 +9,8 @@ const Body = () => {
         </div>
         <div className="res-container">
             {
-                resList.map((restaurant) => (
-                    <RestaurantCard key={restaurant.id} resData={restaurant}/>
+                restaurantList.map((restaurant) => (
+                    <RestaurantCard key={restaurant.data.id} resData={restaurant}/>
                 ))
             }
         </div>
--- src/component/RestaurantCard.jsx
+++ src/component/RestaurantCard.jsx
@@ -1,14 +1,16 @@
+import {CDN_URL} from "../utils/constant";
+
 //3. Restaurant Card
 const RestaurantCard = (props) => {
     const {resData} = props;
 
     return (<div className="res-card">
-        <img className="res-logo" src={resData.image}/>
-        <h3>{resData.name}</h3>
-        <h4>{resData.receipe}</h4>
-        <h4>{resData.rating}</h4>
-        <h4>{resData.deliveryTime}</h4>
-        <h4>{resData.price}</h4>
+        <img className="res-logo" src={CDN_URL+resData.data.cloudinaryImageId}/>
+        <h3>{resData.data.name}</h3>
+        <h4>{resData.data.cuisines.join(", ")}</h4>
+        <h4>{resData.data.avgRating} stars</h4>
+        <h4>{resData.data.deliveryTime} mins</h4>
+        <h4>₹{resData.data.costForTwo/100} FOR TWO</h4>
     </div>);
 }
 
