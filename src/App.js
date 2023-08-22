19c2a85e20037187495763545e5fe3129908acde
Divya <divyareddy0205@gmail.com>
2023-08-22 13:51:16 -0700

    folder-structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import Header from "./component/Header";
-import Body from "./component/Body";
+import Header from "./components/Header";
+import Body from "./components/Body";
 
 /*
     1. Header
