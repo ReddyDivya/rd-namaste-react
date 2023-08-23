0ed1393f333ad73d2518ed8f7aa023b05697d94e
Divya <divyareddy0205@gmail.com>
2023-08-22 19:29:16 -0700

    code

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Header from "./components/Header";
 import Body from "./components/Body";
+import About from "./components/About";
+import Error from "./components/Error";
+import Contacts from "./components/Contacts";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 /*
     1. Header
@@ -23,6 +27,27 @@ const AppLayout = () => {
         <Body/>
     </div>
 }
+
+//Router Configuration
+const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element : <AppLayout/>,
+        errorElement: <Error/>,//shows error page
+    },
+    {
+        path: "/about",
+        element: <About/>,
+    },
+    {
+        path: "/contacts",
+        element: <Contacts/>,
+    },
+])
 const root = createRoot(document.getElementById("root"));
 
-root.render(<AppLayout/>);
+//Providing router configuration(appRouter) to the AppLayout
+root.render(
+<RouterProvider router={appRouter}>
+    <AppLayout/>
+</RouterProvider>);
