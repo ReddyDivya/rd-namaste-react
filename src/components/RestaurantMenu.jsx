abbd8564f631247561e422ed6036b573ec8a1990
Divya <divyareddy0205@gmail.com>
2023-08-23 20:21:42 -0700

    restaurants-info

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import {SWIGGY_MENU_API_URL} from "../utils/constant";
 import { useParams } from "react-router-dom";
+import Shimmer from "./Shimmer";
 
 const RestaurantMenu = () => {
     const {resId} = useParams();//call useParam to get value of restaurant Id(resId) using object destructuring.
@@ -14,33 +15,35 @@ const RestaurantMenu = () => {
 
     //get restaurant details
     const getRestaurantInfo = async () => {
-        const menuData = await fetch(SWIGGY_MENU_API_URL + resId);//fetching menu data
-        const jsonMenuData = await menuData.json();//converting fetched data to json
-        const restaurantData = jsonMenuData?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
+        const restaurantData = await fetch(SWIGGY_MENU_API_URL + resId);//fetching menu data
+        const jsonResData = await restaurantData.json();//converting fetched data to json
         
         //set restaurant
-        setRestaurant(restaurantData);
+        setRestaurant(jsonResData.data);
+
+        // const restaurantData = jsonResData?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
+        
+        
         
         // Set menu item data
-        const menuItemsData = jsonMenuData?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.map(item => item.card.card.itemCards[0].card.info.name);
-        console.log('menuItemsData >> '+ menuItemsData);
+        // const menuItemsData = jsonMenuData?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.map(item => item.card.card.itemCards[0].card.info.name);
+        // console.log('menuItemsData >> '+ menuItemsData);
         
         // console.log('menu >>> ', jsonMenuData);
         // console.log('Data >>> ', jsonMenuData.data.cards[2].groupedCard.cardGroupMap.REGULAR.cards);
-        console.log('Data >>> ', jsonMenuData.data.cards[2].groupedCard.cardGroupMap.REGULAR.cards[3].card.card.itemCards[0].card.info.name);
-        
-       
+        // console.log('Data >>> ', jsonMenuData.data.cards[2].groupedCard.cardGroupMap.REGULAR.cards[3].card.card.itemCards[0].card.info.name);
 
     }
-    
+
+    if(restaurant === null) return <Shimmer/>;
+
+    //destructuring the restaurant info
+    const {name, cuisines, costForTwoMessage} = restaurant?.cards[0]?.card?.card?.info;
+
     return ( 
     <div>
-        <h1>Menu</h1>
-        {
-            // resMenu.map(item => (
-            //     <p>{item}</p>
-            // ))
-        }
+        <h1>{name}</h1>
+        <h2>{cuisines.join(", ")} - {costForTwoMessage}</h2>
     </div>)
 
 }
